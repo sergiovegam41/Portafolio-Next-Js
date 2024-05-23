a0fe361abaa14f181024fa2a88e53048230f9ba3
Justin Tobinson <dantehernande78@gmail.com>
2024-05-22 21:08:19 -0500

    Initial commit

---
+++ components/ui/word-rotate.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { AnimatePresence, HTMLMotionProps, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+interface WordRotateProps {
+  words: string[];
+  duration?: number;
+  framerProps?: HTMLMotionProps<"h1">;
+  className?: string;
+  textColor?: string; // Nueva prop para el color del texto
+}
+
+export default function WordRotate({
+  words,
+  duration = 2500,
+  framerProps = {
+    initial: { opacity: 0, y: -50 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: 50 },
+    transition: { duration: 0.65, ease: "easeOut" },
+  },
+  className,
+  textColor = "black", // Valor predeterminado del color del texto
+}: WordRotateProps) {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, duration);
+
+    // Clean up interval on unmount
+    return () => clearInterval(interval);
+  }, [words, duration]);
+
+  return (
+    <div className="overflow-hidden py-2">
+      <AnimatePresence mode="wait">
+        <motion.h1
+          key={words[index]}
+          className={cn(className)}
+          {...framerProps}
+          style={{ color: "rgb(203 172 249)" }} // Establece el color del texto
+        >
+          {words[index]}
+        </motion.h1>
+      </AnimatePresence>
+    </div>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,194 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Skills", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Some of my tech",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Constant skill improvement",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 7,
+    title: "Tech",
+    description: "Last Technology",
+    className: "md:col-span-full md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "",
+    spareImg: "/grid.svg",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Responsible for implementing the visual elements that users interact with in our web applications. Worked closely with the design team to translate UI/UX designs into code and will collaborate with backend developers to integrate frontend components with logic. server side.",
+    name: "-Frontend Development:",
+    title: "",
+  },
+  {
+    quote:
+      "Responsible for designing, implementing and maintaining server-side logic and databases for our web applications. Worked closely with frontend developers to integrate frontend components with backend services and collaborated with other members of the development team to ensure smooth application performance.",
+    name: "-Backend Development:",
+    title: "",
+  },
+  {
+    quote:
+      "Responsible for designing the software architecture for our applications, guaranteeing the scalability, security and efficiency of the system. I work closely with development teams to define architectural standards and patterns, as well as oversee the implementation of technical solutions.",
+    name: "-Software Architecture:",
+    title: "",
+  },
+  {
+    quote:
+      "Responsible for managing version control using Git, ensuring the integrity and traceability of the project's source code. I work collaboratively with the development team to establish and maintain efficient workflows, manage code branches, and perform merges and conflict resolutions effectively.",
+    name: "-Git version control:",
+    title: "",
+  },
+  {
+    quote:
+      "Responsible for leading or coordinating development teams using the agile Scrum methodology. I work closely with stakeholders to prioritize and plan project iterations, as well as facilitate daily stand-up, sprint planning, sprint review, and sprint retrospective meetings.",
+    name: "-Agile Scrum methodology:",
+    title: "",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Developer ",
+    desc: "Responsible for building the parts of a website or web application that users interact with directly. ",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev ",
+    desc: "Designed and developed mobile app for both iOS & Android platforms.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "UI/ UX Designer",
+    desc: "Design focuses on the visual elements of a digital product. ",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Backend Developer",
+    desc: "Focus on building and maintaining the server-side logic, databases, and APIs that power web applications and websites. ",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const logo = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Asimpl3-Hero",
+  },
+  {
+    id: 2,
+    img: "/whatsapp.svg",
+    link: "https://wa.me/qr/NIPC7MFTSTZ7M1",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/justin-hernandez-sofware-develop/",
+  },
+];
