9bf138906f34ce0d84f2946ae67961433593fca3
Justin Tobinson <dantehernande78@gmail.com>
2024-05-22 22:16:04 -0500

    Updated

--- app/sentry-example-page/page.jsx
+++ app/sentry-example-page/page.jsx
@@ -65,7 +65,7 @@ export default function Page() {
 
         <p>
           Next, look for the error on the{" "}
-          <a href="https://javascript-mastery.sentry.io/issues/?project=4507222371729408">Issues Page</a>.
+          <a href="https://asimpl3-hero.sentry.io/issues/?project=4507303532756992">Issues Page</a>.
         </p>
         <p style={{ marginTop: "24px" }}>
           For more information, see{" "}
--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://57087cd8c7b24fada6c75e6e4499bba2@o4507303423180800.ingest.us.sentry.io/4507303532756992",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
